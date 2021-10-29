11f53bb691a9ad93a039f1ae6ceec42aa35a43af
Anastasia Kikot <kikotnastia18@gmail.com>
2021-10-29 21:42:59 +0300

    commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+    }
+        int balance = 2_000_000_000;
+        int sum = 500_000_000;
+        int result = balance - sum;
+        System.out.println(result);
+    }
